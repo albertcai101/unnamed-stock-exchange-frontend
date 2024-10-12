b051892273758c28357c94e9485bb564e7a43ba3
albertcai101 <albertcai.paloalto@gmail.com>
2024-10-12 16:34:46 -0700

    dummy home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,56 @@
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { Upload } from 'lucide-react';
+import { LaptopIcon } from '@radix-ui/react-icons';
+import { Button } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+import { Separator } from '@/components/ui/separator';
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+export default function Home() {
+
+    const component_header = () => {
+        return (
+            <div className="relative flex w-full flex-col items-start">
+                <section className="mx-auto flex flex-col items-start gap-2 px-4 py-8 md:py-12 md:pb-8 lg:py-12 lg::pb-10 w-full">
+                    <a className="group inline-flex items-center px-0.5 text-sm font-medium" href="https://editor.rive.app/" target="_blank" rel="noopener noreferrer">
+                        <LaptopIcon className="h-4 w-4" />
+                        <Separator orientation="vertical" className="mx-2 h-4" />
+                        <span className="underline-offset-4 group-hover:underline">open rive editor</span>
+                        <ArrowRight className="h-4 w-4 ml-1" />
+                    </a>
+                    <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1] hidden md:block">
+                        Rive Web Runtime, Upgraded.
+                    </h1>
+                    <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1] md:hidden">
+                        Rive Runtime.
+                    </h1>
+                    <p className="max-w-2xl text-lg font-light text-foreground"> Test interactions through animations and the state machine.</p>
+                    <div className="fex w-full items-center justify-start gap-2 py-2">
+                        <div className="flex items-center gap-2">
+                            <a href="https://github.com/albertcai101/rive-rip" target="_blank" rel="noopener noreferrer">
+                                <Button size="xs">
+                                    Star on GitHub
+                                </Button>
+                            </a>
+                            <a href="https://rive.app/preview/" target="_blank" rel="noopener noreferrer">
+                                <Button size="xs" variant="ghost">
+                                    Use Old Rive Preview
+                                </Button>
+                            </a>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        );
+    }
+
+    return (
+        <>
+            <main className="flex-1 font-[family-name:var(--font-geist-sans)]">
+                <div id='container' className="px-8 max-w-[1400px] mx-auto">
+                    { component_header() }
+                </div>
+            </main>
+        </>
+    );
 }
